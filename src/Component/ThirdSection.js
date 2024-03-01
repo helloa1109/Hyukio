855c62dd724535fc1e39a949799f3bd302476485
helloa1109 <helloa1109@naver.com>
2024-03-01 14:44:23 +0900

    fix:프로젝트 소개 디자인 수정

--- src/Component/ThirdSection.js
+++ src/Component/ThirdSection.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import { ProjectBox, ThirdContainer } from '../style/ThirdStyle';
+import React, { useRef, useEffect } from 'react';
+import { BoxWrapper, ProjectBox, TextWrapper, ThirdContainer } from '../style/ThirdStyle';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 function ThirdSection(props) {
+    const BoxRef = useRef(null);
+    const TextRef = useRef(null);
+    const ContainerRef = useRef(null);
+
+    // useEffect(() => {
+    //     // Animation for the text
+    //     gsap.fromTo(
+    //         TextRef.current,
+    //         { opacity: 0, y: 50 },
+    //         { opacity: 1, y: 0, duration: 1, ease: 'power1.in', scrollTrigger: {
+    //             trigger: ContainerRef.current,
+    //             start: 'top 70% center',
+    //             end: 'top 40% center',
+    //             scrub: 1,
+    //             markers: true,
+    //         }}
+    //     );
+
+    //     gsap.fromTo(
+    //         BoxRef.current,
+    //         { opacity: 0, x: -100 },
+    //         { opacity: 1, x: 0, duration: 1, ease: 'power1.in', scrollTrigger: {
+    //             trigger: '.ThirdContainer', 
+    //             start: 'top top',
+    //             end: 'top 40% center',
+    //             scrub: 1,
+    //         }}
+    //     );
+    // }, []);
+
     return (
-        <ThirdContainer>
-            <ProjectBox color="#FEACBF"></ProjectBox>
-            <ProjectBox color="#FEBF5B"></ProjectBox>
-            <ProjectBox color="#A0E57E"></ProjectBox>
+        <ThirdContainer ref={ContainerRef} className='ThirdContainer'>
+            <TextWrapper ref={TextRef}>
+                <span>Tasodkofa</span>
+            </TextWrapper>
+            <BoxWrapper>
+                <ProjectBox color="#FEACBF" className='Back' ref={BoxRef}>
+                    GoodSeul
+                </ProjectBox>
+                <ProjectBox color="#FEBF5B">Wepli</ProjectBox>
+                <ProjectBox color="#A0E57E">DreamStay</ProjectBox>
+            </BoxWrapper>
         </ThirdContainer>
     );
 }
--- src/style/ThirdStyle.js
+++ src/style/ThirdStyle.js
@@ -9,8 +9,9 @@ export const ThirdContainer = styled.div`
     width: 100vw;
     height: 100vh;
     display: flex;
-    justify-content: space-around;
+    justify-content: center;
     align-items: center;
+    flex-direction: column;
 
     @media (max-width: 768px) {
         flex-direction: column;
@@ -23,16 +24,55 @@ export const ThirdContainer = styled.div`
     }
 `;
 
-export const ProjectBox = styled.div`
-    width: 440px;
-    height: 440px;
-    border-radius: 400px;
+export const BoxWrapper = styled.div`
+    display: flex;
+`;
 
+export const TextWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    top: -20%;
+    width: 100%;
+
+    span{ 
+        font-family: "Poppins";
+        font-weight: 600;
+        font-style: normal;     
+        font-stretch: normal;
+        font-size: 60px;
+        letter-spacing: 40px;
+    }
+`;
+
+export const ProjectBox = styled.div`
+    position: relative;
+    width: 320px;
+    height: 320px;
+    border-radius: 400px;
+    border: 1px solid black;
     ${({ color }) => color && `
         background: radial-gradient(circle, ${color} 0%, transparent 70%);
     `}
-
     backdrop-filter: blur(300px);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    position: relative;
+
+    /*
+        폰트 설정
+    */
+    font-family: "Poppins";
+    color: white; 
+    text-align: center;
+    font-size: 40px; 
+    font-weight: 600;
+    font-style: normal; 
+    font-stretch: normal;
+    letter-spacing: 0.2px;
 
     @media (max-width: 768px) {
         width: 50vw;
