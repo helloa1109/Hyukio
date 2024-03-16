9f18c2345ded6f94f8dbcb71095aad2ae749724c
helloa1109 <helloa1109@naver.com>
2024-03-16 15:02:38 +0900

    feat : 네번째 레이아웃

--- src/Component/FourSection.js
+++ src/Component/FourSection.js
@@ -1,14 +1,64 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { styled } from 'styled-components';
 
 function FourSection(props) {
+    const scrollableWrapperRef = useRef(null);
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    const handleScroll = (e) => {
+        if (scrollableWrapperRef.current) {
+            scrollableWrapperRef.current.scrollLeft += e.deltaY; // Translate vertical scrolling to horizontal scrolling
+            setScrollPosition(scrollableWrapperRef.current.scrollLeft);
+        }
+    };
+
+    useEffect(() => {
+        const handleScrollEnd = () => {
+            const wrapper = scrollableWrapperRef.current;
+            if (wrapper) {
+                const scrollThreshold = wrapper.clientWidth / 2; // Threshold for considering reaching the previous component
+                if (scrollPosition <= scrollThreshold) {
+                    wrapper.scrollTo({ left: 0, behavior: 'smooth' }); // Scroll to the beginning of the wrapper
+                }
+            }
+        };
+
+        window.addEventListener('scroll', handleScrollEnd);
+
+        return () => {
+            window.removeEventListener('scroll', handleScrollEnd);
+        };
+    }, [scrollPosition]);
+
     return (
-        <Container>
-            <Card>
-                <CardText>R</CardText>
-                <CardTitle>리액트를 사용하여 프로젝트</CardTitle>
-                <CardSub>상태관리에 대하여 작성하였습니다.</CardSub>
-            </Card>
+        <Container onWheel={handleScroll}>
+            <ScrollableWrapper ref={scrollableWrapperRef}>
+                <Wrapper>
+                    <Card>
+                        <CardText>R</CardText>
+                        <CardTitle>리액트를 사용</CardTitle>
+                        <CardSub>상태관리에 대하여 작성하였습니다.</CardSub>
+                    </Card>
+
+                    <Card>
+                        <CardText>T</CardText>
+                        <CardTitle>타입스크립트를 사용</CardTitle>
+                        <CardSub>상태관리에 대하여 작성하였습니다.</CardSub>
+                    </Card>
+
+                    <Card>
+                        <CardText>S</CardText>
+                        <CardTitle>스타일컴포넌트 사용</CardTitle>
+                        <CardSub>상태관리에 대하여 작성하였습니다.</CardSub>
+                    </Card>
+
+                    <Card>
+                        <CardText>S</CardText>
+                        <CardTitle>스타일컴포넌트 사용</CardTitle>
+                        <CardSub>상태관리에 대하여 작성하였습니다.</CardSub>
+                    </Card>
+                </Wrapper>
+            </ScrollableWrapper>
         </Container>
     );
 }
@@ -20,6 +70,20 @@ const Container = styled.div`
     display: flex;
     align-items: center;
     justify-content: center;
+    overflow: hidden; /* Ensure overflow content is hidden */
+`;
+
+const ScrollableWrapper = styled.div`
+    width: 100%;
+    overflow-x: auto; 
+    padding: 20px;
+`;
+
+const Wrapper = styled.div`
+    width: 300vw; 
+    display: flex;
+    align-items: flex-start;
+    flex-direction: row;
 `;
 
 const Card = styled.div`
@@ -28,17 +92,17 @@ const Card = styled.div`
     border-radius: 34px;
     background-color: white;
     display: flex;
-    flex-direction: column;
-    
+    flex-direction: column; 
+    margin-right: 20px; 
 `;
 
-const CardText =styled.span`
+const CardText = styled.span`
     font-family: 'Anticva';
     font-size: 140px;
     margin: 20px;
 `;
 
-const CardTitle =styled.span`
+const CardTitle = styled.span`
     font-family: "Poppins", "Pretendard";
     font-size: 34px;
     border: 1px solid red;
