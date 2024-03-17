09f418c0bf5b7e0efe45206b1a27bf9a83fcc5f2
helloa1109 <helloa1109@naver.com>
2024-03-17 14:05:31 +0900

    fix: css

--- src/App.js
+++ src/App.js
@@ -3,21 +3,26 @@ import './App.css';
 import FirstSection from './Component/FirstSection';
 import SecondSection from "./Component/SecondSection";
 import ThirdSection from './Component/ThirdSection';
-import Navbar from './Component/Navbar';
 import Footer from './Component/FiveSection';
 import FourSection from './Component/FourSection';
+import { styled } from 'styled-components';
 
 function App() {
 
   return (
-    <>
+    <Page>
       <FirstSection />
       <SecondSection />
       <ThirdSection />
       <FourSection/>
       <Footer/>
-    </>
+    </Page>
   );
 }
+
+const Page = styled.div`
+  width:100%;
+  overflow-x:hidden;
+`;
 
 export default App;
--- src/Component/FourSection.js
+++ src/Component/FourSection.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState, useEffect } from 'react';
 import { styled } from 'styled-components';
+import "../style/FourSection.css";
 
 function FourSection(props) {
     const scrollableWrapperRef = useRef(null);
@@ -7,7 +8,7 @@ function FourSection(props) {
 
     const handleScroll = (e) => {
         if (scrollableWrapperRef.current) {
-            scrollableWrapperRef.current.scrollLeft += e.deltaY; // Translate vertical scrolling to horizontal scrolling
+            scrollableWrapperRef.current.scrollLeft += e.deltaY;
             setScrollPosition(scrollableWrapperRef.current.scrollLeft);
         }
     };
@@ -16,9 +17,9 @@ function FourSection(props) {
         const handleScrollEnd = () => {
             const wrapper = scrollableWrapperRef.current;
             if (wrapper) {
-                const scrollThreshold = wrapper.clientWidth / 2; // Threshold for considering reaching the previous component
+                const scrollThreshold = wrapper.clientWidth / 2;
                 if (scrollPosition <= scrollThreshold) {
-                    wrapper.scrollTo({ left: 0, behavior: 'smooth' }); // Scroll to the beginning of the wrapper
+                    wrapper.scrollTo({ left: 0, behavior: 'smooth' });
                 }
             }
         };
@@ -32,27 +33,28 @@ function FourSection(props) {
 
     return (
         <Container onWheel={handleScroll}>
+            {/* <HeaderText>Skill</HeaderText> */}
             <ScrollableWrapper ref={scrollableWrapperRef}>
-                <Wrapper>
-                    <Card>
+                <Wrapper className='Slide'>
+                    <Card style={{ marginTop: '50px' }}>
                         <CardText>R</CardText>
                         <CardTitle>리액트를 사용</CardTitle>
                         <CardSub>상태관리에 대하여 작성하였습니다.</CardSub>
                     </Card>
 
-                    <Card>
+                    <Card style={{ marginTop: '30px' }}>
                         <CardText>T</CardText>
                         <CardTitle>타입스크립트를 사용</CardTitle>
                         <CardSub>상태관리에 대하여 작성하였습니다.</CardSub>
                     </Card>
 
-                    <Card>
+                    <Card style={{ marginTop: '70px' }}>
                         <CardText>S</CardText>
                         <CardTitle>스타일컴포넌트 사용</CardTitle>
                         <CardSub>상태관리에 대하여 작성하였습니다.</CardSub>
                     </Card>
 
-                    <Card>
+                    <Card style={{ marginTop: '20px' }}>
                         <CardText>S</CardText>
                         <CardTitle>스타일컴포넌트 사용</CardTitle>
                         <CardSub>상태관리에 대하여 작성하였습니다.</CardSub>
@@ -64,19 +66,34 @@ function FourSection(props) {
 }
 
 const Container = styled.div`
-    width: 100vw;
+    width: 100%;
     height: 100vh;
     background-color: #090FC1;
     display: flex;
     align-items: center;
     justify-content: center;
-    overflow: hidden; /* Ensure overflow content is hidden */
+    overflow: hidden;
+    position: relative;
+`;
+
+const HeaderText = styled.div`
+    position: absolute;
+    top: 20px;
+    left: 20%;
+    transform: translateX(-50%);
+    font-size: 100px;
+    color: white;
+    font-family: "Poppins";
 `;
 
 const ScrollableWrapper = styled.div`
     width: 100%;
     overflow-x: auto; 
     padding: 20px;
+    
+    .Slide::-webkit-scrollbar{
+        display: none;
+    }
 `;
 
 const Wrapper = styled.div`
@@ -87,8 +104,8 @@ const Wrapper = styled.div`
 `;
 
 const Card = styled.div`
-    width: 500px;
-    height: 700px;
+    width: 400px;
+    height: 600px;
     border-radius: 34px;
     background-color: white;
     display: flex;
@@ -112,7 +129,9 @@ const CardTitle = styled.span`
 const CardSub = styled.span`
     font-family: "Poppins";
     font-size: 20px;
-    margin-left: 20px;
+    margin: 20px;
+    border: 1px solid red;
+
 `;
 
 export default FourSection;
